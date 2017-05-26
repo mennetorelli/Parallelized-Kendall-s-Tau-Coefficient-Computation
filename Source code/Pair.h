a0a5be31feeb6eedeff9f8cfff64ee036a819afe
MatteoPenco <matteo.penco05@gmail.com>
2017-05-26 13:00:14 +0200

    commit

---
+++ Source code/Pair.h
@@ -0,0 +1,17 @@
+class Pair
+{
+public:
+	Pair(double first, double second);
+	~Pair();
+
+	void setFirst(double n);
+	double getFirst();
+	
+	void setSecond(double n);
+	double getSecond();
+
+private:
+	double first;
+	double second;
+};
+
