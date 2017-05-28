d148e8d5f64bcbabbe904187267a63f6b280235d
Emanuele Torelli <mennetorelli@hotmail.it>
2017-05-28 11:34:25 +0200

    aggiustamenti

--- Source code/Step3.cpp
+++ Source code/Step3.cpp
@@ -9,48 +9,45 @@ Step3::~Step3()
 }
 
 
-int Step3::step3_merge(vector<Pair> &in, vector<Pair> &out, int left, int mid, int right) {
+int Step3::step3_merge(vector<Pair> &input, vector<Pair> &buffer, int left, int mid, int right) {
 	int l = left;
 	int p = left;
 	int r = mid;
 	int nd = 0;
 	while (l < mid && r < right) {
-		if (in[r].getSecond() < in[l].getSecond()) {
+		if (input[r].getSecond() < input[l].getSecond()) {
 			nd += mid - l;
-			out[p++] = in[r++];
+			buffer[p++] = input[r++];
 		}
 		else
-			out[p++] = in[l++];
+			buffer[p++] = input[l++];
 	}
-	while (l < mid) 
-		out[p++] = in[l++];
+	while (l < mid)
+		buffer[p++] = input[l++];
 	while (r < right) {
 		nd += mid - l;
-		out[p++] = in[r++];
+		buffer[p++] = input[r++];
 	}
 	return nd;
 }
 
 int Step3::step3_divide(vector<Pair> &input, vector<Pair> &buffer, int n) {
-	vector<Pair> in = input;
-	vector<Pair> out = buffer;
 	int nd = 0;
 	for (int s = 1; s < n; s *= 2) {
 		for (int l = 0; l < n; l += 2 * s) {
 			int m = min(l + s, n);
 			int r = min(l + 2 * s, n);
-			nd += Step3::step3_merge(in, out, l, m, r);
+			nd += Step3::step3_merge(input, buffer, l, m, r);
 		}
-	swap(in, out);
+	swap(input, buffer);
 	}
-	input = in;
 	return nd;
 }
 
-/*int main()
+int main()
 {
 	Step3 step3;
-	vector<Pair> input = { Pair(1, 2), Pair(2, 2), Pair(2, 3), Pair(3, 1) };
+	vector<Pair> input = { Pair(1, 2), Pair(1, 3), Pair(2, 1), Pair(2, 2), Pair(2, 3), Pair(2, 4),  Pair(4, 1), Pair(4, 4) };
 	int n = input.size();
 	for (int i = 0; i < n; i++) {
 		cout << "(" << input[i].getFirst() << "," << input[i].getSecond() << ") ";
@@ -65,4 +62,4 @@ int Step3::step3_divide(vector<Pair> &input, vector<Pair> &buffer, int n) {
 	cout << nd << endl;
 	system("pause");
 	return 0;
-}*/
+}
